25b649a86a407dbb5856d6b36bf5fb6650f16ad2
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-09-29 09:41:35 +0200

    MainPage

---
+++ src/features/MainPage/index.js
@@ -0,0 +1,43 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchGithubData, selectPortfolioList, selectLoadingState } from "../DataSlice/githubSlice";
+import Projects from "./Projects";
+
+const MainPage = () => {
+
+  const dispatch = useDispatch();
+  const mainPage = useSelector(selectPortfolioList);
+
+  useEffect(() => {
+    dispatch(fetchGithubData());
+  }, []);
+
+  let returned = "";
+
+  switch (mainPage.ifLoading) {
+    case "loading":
+      returned = (
+<p>ładujemy gnojka</p>
+       // <Loading />
+      );
+      break;
+    case "success":
+      returned = (
+        <>
+     <p>sukces!</p>
+     <Projects projects={mainPage.portfolioList}/>
+     </>
+      );
+      break;
+    default:
+        console.log(mainPage.ifLoading);
+        returned =<p>error!</p>;
+      //returned = <ErrorPage />;
+  }
+
+  return returned;
+
+};
+
+export default MainPage;
