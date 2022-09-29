0f2721ac8d1cd092a53963af88e1b6d8a25f0a5e
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-09-29 09:42:07 +0200

    redux init (slice)

---
+++ src/features/DataSlice/githubSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const githubSlice = createSlice({
+  name: "portfolioList",
+  initialState: {
+    portfolioList: [],
+    ifLoading: "",
+  },
+  reducers: {
+    fetchGithubData: (state) => {
+      state.ifLoading = "loading";
+      console.log(state.ifLoading);
+    },
+    setPortfolioList: (state, { payload: data}) => {
+      state.portfolioList = data;
+      state.ifLoading = "success";
+      console.log(state.portfolioList);
+    },
+  },
+});
+
+export const {
+fetchGithubData,
+setPortfolioList
+} = githubSlice.actions;
+
+export const selectPortfolioList = (state) => state.portfolioList;
+export const selectLoadingState = (state) => state.ifLoading;
+// export const getMovieById = (state, movieId) =>
+//   selectMoviesDetails(state).movieDetails.find(({ id }) => id === movieId);
+
+export default githubSlice.reducer;
