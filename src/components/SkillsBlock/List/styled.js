2f7cb65947e42e58e88055f1fb1267a04c462f10
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-02 22:16:45 +0200

    some more effects, color transition to tiles

--- src/components/SkillsBlock/List/styled.js
+++ src/components/SkillsBlock/List/styled.js
@@ -22,20 +22,25 @@ export const ListHeader = styled.h2`
 export const StyledList = styled.ul`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    padding: 0;
-    margin: 24px 0 0;
+    padding: 0px;
+    column-gap: 15px;
     list-style-type: none;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoint.miniMobile}px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 `;
 
 export const StyledMarker = styled(Marker)`
     margin-right: 16px;
     flex-shrink: 0;
     & circle {
-        fill: black;
+        fill: #0366D6;
     }
 
 `;
-
 export const ListItem = styled.li`
     display: inline-flex;
     align-items: center;
@@ -44,6 +49,9 @@ export const ListItem = styled.li`
     line-height: 140%;
     letter-spacing: 0.05em;
     padding: 4px;
-    color: black;
+    color:  ${({ theme }) => theme.color.grey};
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        font-size: 14px;
+    }
 `;
 
