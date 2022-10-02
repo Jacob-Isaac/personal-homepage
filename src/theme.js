fe5fbec0ff7e2e50ef3eefa8dfcac0da24679d58
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-02 22:14:50 +0200

    new themes added

--- src/theme.js
+++ src/theme.js
@@ -3,16 +3,18 @@ export const theme = {
     color: {
         white: "#FFFFFF",
         black: "#252525",
-        gray: "#6E7E91",
+        grey: "#6E7E91",
         background: "#E5E5E5",
+        bodyBackground: "#FBFBFE",
         mainBlue: "#0366D6",
+        borderBottom: "#D1D5DA4D",
     },
 
     breakpoint: {
         maxContentSize: 1368,
         tablet: 1060,
         mobile: 800,
-        between: 700,
+        miniMobile: 500,
     },
     fontSize: {
         regular: 400,
