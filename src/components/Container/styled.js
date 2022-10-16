28a42f710532605f3edb13bb2f1f02696e6b20da
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-16 19:55:09 +0200

    rest of the project improved look for mobile

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -4,4 +4,7 @@ export const Container = styled.div`
 max-width: 1216px;
 margin: auto;
 min-width: 280px;
+@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+width:90%
+  }
 `;
--- src/components/Tiles/styled.js
+++ src/components/Tiles/styled.js
@@ -1,109 +1,101 @@
 import styled from "styled-components";
 
-
 export const Tiles = styled.div`
-max-width: 586px;
-background-color:  ${({theme}) => theme.color.white};
-border: 6px solid rgba(209, 213, 218, 0.3);
-box-shadow: 0px 0px 10px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-border-radius: 4px;
-transition: border-color 1s cubic-bezier(0.69, 0.15, 0.31, 0.85);
-&:hover {
-        border-color: #0366D633;
-    }
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  max-width: 586px;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 6px solid rgba(209, 213, 218, 0.3);
+  box-shadow: 0px 0px 10px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+  transition: border-color 1s cubic-bezier(0.69, 0.15, 0.31, 0.85);
+  &:hover {
+    border-color: #0366d633;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     width: 100%;
-    min-width: 280px;
-    }
-`;
-
-export const UlWrapper = styled.div`
-margin-right:40px;
-margin-bottom: 30px;
-margin-top: 20px;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    margin-top: 0px;
-    margin-bottom: 5px;
-    margin-right:10px;
-    }
+  }
 `;
 
 export const StyledUl = styled.ul`
-list-style-type: none;
-display: inline-flex;
-flex-wrap: wrap;
-flex-direction: column;
-gap: 22px;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    padding: 10px;
-    }
+  list-style-type: none;
+  padding-right: 40px;
+  padding-bottom: 40px;
+  padding-top: 30px;
+  display: inline-flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 22px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 0px;
+    padding-bottom: 10px;
+  }
 `;
 
 export const Name = styled.li`
-font-weight: ${({theme}) => theme.fontSize.bold};
-font-size: 24px;
-line-height: 29px;
-letter-spacing: 0.05em;
-color: #0366D6;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  font-weight: ${({ theme }) => theme.fontSize.bold};
+  font-size: 24px;
+  line-height: 29px;
+  letter-spacing: 0.05em;
+  color: #0366d6;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 16px;
-    }
-
+  }
 `;
 
 export const Description = styled.li`
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 140%; 
-letter-spacing: 0.05em;
-text-align: justify;
-color: #6E7E91;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
+  text-align: justify;
+  color: #6e7e91;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 14px;
-    padding-right: 20px;
-    }
+    padding-right: 10px;
+    text-align: left;
+  }
 `;
 
 export const Links = styled.li`
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 140%;
-letter-spacing: 0.05em;
-color: #6E7E91;
-display: flex;
-flex-direction: column;
-gap:8px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
+  color: #6e7e91;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
 
 export const Demo = styled.span`
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 14px;
-    }
+  }
 `;
 
 export const Code = styled.span`
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 14px;
     margin-right: 10px;
-    }
+  }
 `;
 
 export const Link = styled.a`
-    color: #0366D6;
-    text-decoration: none;
-    border-bottom: 1px solid #BAD0E8;
-    padding-bottom: 1px;
-    &:hover {
-        border-bottom: 1px solid #0366D6;
-        /* border-color: unset; */
-    }
-    /* α(#0366D6, 0.3); */
+  color: #0366d6;
+  text-decoration: none;
+  border-bottom: 1px solid #bad0e8;
+  padding-bottom: 1px;
+  &:hover {
+    border-bottom: 1px solid #0366d6;
+  }
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 14px;
-    }
+  }
 `;
