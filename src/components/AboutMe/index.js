8018c37c458505fc7abf88d42464ce8c41652dd2
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-30 20:21:54 +0100

    button animation

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,18 +1,20 @@
-import { IntroBlock, MyPhoto, ModeChange, Text, Name, Article, HireMeButton, IconBody, Body, DarkMode, StyledButtonImg  } from "./styled";
+import { IntroBlock, MyPhoto, ModeChange, Text, Name, Article, HireMeButton, IconBody, Body, DarkMode, StyledButtonImg, StyledSunIcon  } from "./styled";
 import me from "../../images/me.png"
 import { useSelector, useDispatch } from "react-redux";
 import { changeTheme } from "../../features/DataSlice/githubSlice";
+import {selectIsLightMode } from "../../features/DataSlice/githubSlice"
 
- import { ReactComponent as SunIcon } from "../../images/SunIcon.svg";
 
 function AboutMe() {
 
   const dispatch = useDispatch();
+  const theme = useSelector(selectIsLightMode);
+
   
   return (
     <IntroBlock>
     <MyPhoto src={me}/>
-    <ModeChange><DarkMode>DARK MODE OFF</DarkMode><IconBody onClick={() => dispatch(changeTheme())}><Body><SunIcon/></Body></IconBody></ModeChange>
+    <ModeChange><DarkMode>DARK MODE {theme === false ? "ON" : "OFF"}</DarkMode><IconBody onClick={() => dispatch(changeTheme())}><Body><StyledSunIcon $themes={theme}/></Body></IconBody></ModeChange>
    <Text>THIS IS</Text> 
      <Name>Jakub Nowakowski</Name> 
     <Article>⭐✌️ I'm passionate Frontend Developer in love with React, currently looking for new job opportunities.
--- src/components/AboutMe/styled.js
+++ src/components/AboutMe/styled.js
@@ -1,89 +1,82 @@
-import styled, {css} from "styled-components";
-import { ReactComponent as ButtonImg} from '../../images/hireMe.svg';
+import styled, { css } from "styled-components";
+import { ReactComponent as ButtonImg } from "../../images/hireMe.svg";
+import { ReactComponent as SunIcon } from "../../images/SunIcon.svg";
 
-export const Block = styled.div`
-height: 404px;
-margin: auto;
-background-color:  ${({theme}) => theme.color.tilesBackground};
-margin-bottom: 72px;
-margin-top: 72px;
-box-shadow: 0px 0px 20px rgba(9, 10, 51, 0.02), 0px 16px 20px rgba(9, 10, 51, 0.03);
-border-radius: 4px;
+export const StyledSunIcon = styled(SunIcon)`
+  transition: transform 0.6s;
+  ${({ $themes }) => $themes && css`
+      transform: translateX(100%);
+    `};
 `;
 
-
 export const IntroBlock = styled.div`
-   margin-top: 115px;
-margin-bottom: 115px;
-max-height: 404px;
-display: grid;
-grid-template-columns: 1fr 2fr;
-grid-template-rows: 1fr 1fr 1fr 2fr 3fr;
- grid-template-areas:
-      "photo mode "
-      "photo txt "
-      "photo myName "
-      "photo article "
-      "photo hireButton ";
-      grid-row-gap: 10px;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-  grid-template-columns: 1fr;
-grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
- grid-template-areas:
+  margin-top: 115px;
+  margin-bottom: 115px;
+  max-height: 404px;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: 1fr 1fr 1fr 2fr 3fr;
+  grid-template-areas:
+    "photo mode "
+    "photo txt "
+    "photo myName "
+    "photo article "
+    "photo hireButton ";
+  grid-row-gap: 10px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas:
       "mode"
-      "photo" 
+      "photo"
       "txt"
       "myName"
       "article"
       "hireButton";
-      margin-top: 0px;
+    margin-top: 0px;
   }
 `;
 
 export const MyPhoto = styled.img`
-grid-area: photo;
-max-width: 398px;
-border-radius: 50%;
-margin-right: 66px;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    width:70%;
-    min-width:202px;
-    align-self:start;
-    width:40%;
+  grid-area: photo;
+  max-width: 398px;
+  border-radius: 50%;
+  margin-right: 66px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 70%;
+    min-width: 202px;
+    align-self: start;
+    width: 40%;
   }
   @media (max-width: ${({ theme }) => theme.breakpoint.miniMobile}px) {
-     min-width:150px; 
-    width:50%;
+    min-width: 150px;
+    width: 50%;
   }
 `;
 
-
-
 export const Text = styled.div`
-grid-area: txt;
-align-self: end;
-color:  ${({theme}) => theme.color.text};
-font-weight: 700;
-font-size: 12px;
-line-height: 130%;
-text-transform: uppercase;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  grid-area: txt;
+  align-self: end;
+  color:  ${({ theme }) => theme.color.text};
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 130%;
+  text-transform: uppercase;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 10px;
   }
 `;
 
 export const Name = styled.span`
-grid-area: myName;
-align-self: end;
-color:  ${({theme}) => theme.color.titleAndIcons};
-font-weight: 900;
-/* font-size: 38px; */
-/* line-height: 46px; */
-letter-spacing: 0.05em;
-animation: fadeInAnimation ease 4s;
-animation-iteration-count: 1;
-animation-fill-mode: forwards;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  grid-area: myName;
+  align-self: end;
+  color: ${({ theme }) => theme.color.titleAndIcons};
+  font-weight: 900;
+  letter-spacing: 0.05em;
+  animation: fadeInAnimation ease 4s;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 24px;
   }
   @media (min-width: ${({ theme }) => theme.breakpoint.mobile}px) {
@@ -92,149 +85,96 @@ animation-fill-mode: forwards;
   @media (min-width: ${({ theme }) => theme.breakpoint.extra}px) {
     font-size: 38px;
   }
-@keyframes fadeInAnimation {
+  @keyframes fadeInAnimation {
     0% {
-        opacity: 0;
+      opacity: 0;
     }
     100% {
-        opacity: 1;
-     }
-}
+      opacity: 1;
+    }
+  }
 `;
 
 export const Article = styled.article`
-grid-area: article;
-align-self: center;
-max-width: 630px;
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 140%;
-letter-spacing: 0.05em;
-color:  ${({theme}) => theme.color.text};
-text-align: justify;
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+  grid-area: article;
+  align-self: center;
+  max-width: 630px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
+  color: ${({ theme }) => theme.color.text};
+  text-align: justify;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     font-size: 16px;
-    width:90%;
+    width: 90%;
     align-self: center;
   }
 `;
 
-// export const HireMeButton = styled.div`
-
-// margin-top: 60px;
-// width: 154px;
-// height: 49px;
-// border-radius: 4px;
-// `;
-
-// export const HireMeButton = styled(HireButton)`
-// margin-top: 60px;
-
-// &:hover {
-//     box-shadow: 2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;
-//     border-radius: 4px;
-// }
-// &:active {
-//     content: url("../../images/Button-selected-LM.svg");
-// }
-
-// `;
 
 export const HireMeButton = styled.a`
-grid-area: hireButton;
-padding-top: 15px;
-/* margin-top: 60px; */
-     /* padding-top: 14px;  */
-     width: 154px;
- height: 49px;
- /* &:hover{
-        box-shadow: 2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;
-        cursor: pointer;
-        transition: 0.6s;
-    } */
-    /* border-radius: 4px; */
-        @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    /* align-self: center; */
-    /* width: 110px;
- height: 30px; */
+  grid-area: hireButton;
+  padding-top: 15px;
+  width: 154px;
+  height: 49px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
   }
-
 `;
 
 export const StyledButtonImg = styled(ButtonImg)`
-/* grid-area: hireButton; */
- width: 154px;
- height: 49px;
- border-radius: 4px;
- &:hover{
-        box-shadow: 2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;
-        cursor: pointer;
-        transition: 0.6s;
-
-    }
-   
-        @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    /* align-self: center; */
-    width: 138px;
-  height: 45px;
+  width: 154px;
+  height: 49px;
+  border-radius: 4px;
+  &:hover {
+    box-shadow: 2px -2px 0px #8cc2ff, -2px 2px 0px #8cc2ff, 2px 2px 0px #8cc2ff,
+      -2px -2px 0px #8cc2ff;
+    cursor: pointer;
+    transition: 0.6s;
   }
 
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 138px;
+    height: 45px;
+  }
 `;
 
-
-
-
-
 export const ModeChange = styled.div`
-grid-area: mode;
-
-display: flex;
-     flex-direction: row;
-     justify-content: flex-end;
-    
-/* line-height: 130%; */
-/* display: flex; */
-/* align-items: center; */
-
-/* flex: none; */
-/* order: 0;
-flex-grow: 0; */
-@media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-    /* margin-top:15px;
-    margin-bottom:15px; */
+  grid-area: mode;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     align-self: center;
     margin-bottom: -45px;
   }
 `;
 export const DarkMode = styled.div`
-     color: #6E7E91;
-     font-weight: 700;
-     font-size: 12px;
-     display: flex;
-     flex-direction: row-reverse;
-     gap: 12px;
-     padding: 7px;
-     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
- display:none;
+  color: #6e7e91;
+  font-weight: 700;
+  font-size: 12px;
+  display: flex;
+  flex-direction: row-reverse;
+  gap: 12px;
+  padding: 7px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    display: none;
   }
-
-
 `;
 
 export const IconBody = styled.button`
-
-     width: 47px;
-     height: 27px;
-     padding: 2px;
-     border: 1px solid  #6E7E91;
-     border-radius: 15px;
-     background: ${({theme}) => theme.color.background};
-     transition: background-color 1s cubic-bezier(0.69, 0.15, 0.31, 0.85);
-     &:hover{
-          cursor: pointer;
-     }
+  width: 47px;
+  height: 27px;
+  padding: 2px;
+  border: 1px solid #6e7e91;
+  border-radius: 15px;
+  background: ${({ theme }) => theme.color.background};
+  transition: background-color 1s cubic-bezier(0.69, 0.15, 0.31, 0.85);
+  &:hover {
+    cursor: pointer;
+  }
 `;
 export const Body = styled.span`
   display: flex;
@@ -242,26 +182,4 @@ export const Body = styled.span`
   justify-content: center;
   width: 20px;
   height: 20px;
-  border-radius: 50%;
-  transition: transform .6s;
-  background:  ${({theme}) => theme.color.background};
-  
-  ${({ iconTransition }) =>
-          iconTransition && css`
-          transform: translateX(20px);
-          & path {
-            fill: ${({theme}) => theme.color.background};
-        }
-    `};
 `;
-
-
-
-
-    
-  
-   
-    
-    
-
-   
