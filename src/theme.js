6a4f9974e73bfddf77bb6e124b56cffa8507509b
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-30 15:46:52 +0100

    added darkTheme

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,35 @@
-
-export const theme = {
+export const lightTheme = {
     color: {
         white: "#FFFFFF",
         black: "#252525",
         grey: "#6E7E91",
         background: "#E5E5E5",
         bodyBackground: "#FBFBFE",
+        mainBlue: "#0366D6",
+        borderBottom: "#D1D5DA4D",
+    },
+
+    breakpoint: {
+        maxContentSize: 1368,
+        tablet: 1060,
+        mobile: 800,
+        miniMobile: 500,
+        extra: 878,
+    },
+    fontSize: {
+        regular: 400,
+        medium: 500,
+        bold: 700,
+    },
+};
+
+export const darkTheme = {
+    color: {
+        white: "#FFFFFF",
+        black: "#252525",
+        grey: "#6E7E91",
+        background: "#252525",
+        bodyBackground: "#252525",
         mainBlue: "#0366D6",
         borderBottom: "#D1D5DA4D",
     },
