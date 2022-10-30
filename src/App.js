f2854217849b2640262150e784001335a3782a40
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-30 15:47:17 +0100

    index and App changed to allow switching themes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import './App.css';
-import { Container } from './components/Container/styled';
-import SkillsBlock from './components/SkillsBlock/index';
-import AboutMe from './components/AboutMe';
-import { skillsList, toLearnList } from '../src/components/SkillsBlock/List/contents.js';
-import MainPage from './features/MainPage';
-import Footer from "./components/Footer"
+import React from "react";
+import "./App.css";
+import { Container } from "./components/Container/styled";
+import SkillsBlock from "./components/SkillsBlock/index";
+import AboutMe from "./components/AboutMe";
+import { skillsList, toLearnList } from "../src/components/SkillsBlock/List/contents.js";
+import MainPage from "./features/MainPage";
+import Footer from "./components/Footer";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import { selectIsLightMode } from "../src/features/DataSlice/githubSlice";
+import { useSelector } from "react-redux";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
-  console.log(skillsList);
+  const lightMode = useSelector(selectIsLightMode);
   return (
-  <Container>
-    <AboutMe/>
-    <SkillsBlock 
-    title={"My skillset includes 🛠️"}
-    content={skillsList}
-    />
-    <SkillsBlock content={toLearnList} title={"What i want to learn next 🚀"}/>
-    <MainPage/>
-    <Footer/>
-  </Container>
+    <ThemeProvider theme={lightMode === true ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Container>
+        <AboutMe />
+        <SkillsBlock title={"My skillset includes 🛠️"} content={skillsList} />
+        <SkillsBlock
+          content={toLearnList}
+          title={"What i want to learn next 🚀"}
+        />
+        <MainPage />
+        <Footer />
+      </Container>
+    </ThemeProvider>
   );
 }
 
