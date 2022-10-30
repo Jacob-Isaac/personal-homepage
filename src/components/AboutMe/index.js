eb51c7b1bb0695a87aabb33c73ac2e1247e1ee9a
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-30 15:48:14 +0100

    onClick added to change themeMode in redux

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,14 +1,18 @@
 import { IntroBlock, MyPhoto, ModeChange, Text, Name, Article, HireMeButton, IconBody, Body, DarkMode, StyledButtonImg  } from "./styled";
 import me from "../../images/me.png"
+import { useSelector, useDispatch } from "react-redux";
+import { changeTheme } from "../../features/DataSlice/githubSlice";
 
  import { ReactComponent as SunIcon } from "../../images/SunIcon.svg";
 
 function AboutMe() {
+
+  const dispatch = useDispatch();
   
   return (
     <IntroBlock>
     <MyPhoto src={me}/>
-    <ModeChange><DarkMode>DARK MODE OFF</DarkMode><IconBody><Body><SunIcon/></Body></IconBody></ModeChange>
+    <ModeChange><DarkMode>DARK MODE OFF</DarkMode><IconBody onClick={() => dispatch(changeTheme())}><Body><SunIcon/></Body></IconBody></ModeChange>
    <Text>THIS IS</Text> 
      <Name>Jakub Nowakowski</Name> 
     <Article>⭐✌️ I'm passionate Frontend Developer in love with React, currently looking for new job opportunities.
